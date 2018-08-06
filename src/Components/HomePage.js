79b8eedc4a3ff619fa71013cbd87d78f6044af7c
Alon Peleg <peleg619@gmail.com>
2018-08-06 16:33:40 +0300

    step counter working (refresh expo after a save)

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text ,TouchableOpacity,Dimensions} from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions } from 'react-native';
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -17,8 +17,8 @@ export default class HomePage extends Component {
         return (
             <View>
                 <TouchableOpacity
-                style={{position:'absolute',top:HEIGHT/2,left:WIDTH/2,height:35,width:100,alignItems:'center',justifyContent: 'center',}}
-                onPress={()=>{this.props.navigation.navigate("steps")}}
+                    style={{ position: 'absolute', top: HEIGHT / 2, left: WIDTH - 250, height: 35, width: 100, alignItems: 'center', justifyContent: 'center', }}
+                    onPress={() => { this.props.navigation.navigate("steps") }}
                 >
                     <Text>Count My Steps</Text>
                 </TouchableOpacity>
--- src/Components/StepCounter.js
+++ src/Components/StepCounter.js
@@ -1,21 +1,92 @@
-import React, { Component } from 'react';
-import {View,Text} from 'react-native';
+import Expo from "expo";
+import React from "react";
+import { Pedometer } from "expo";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default class StepCounter extends Component{
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         
-      };
+export default class StepCounter extends React.Component {
+    static navigationOptions = {
+        header: null,
     };
 
+    state = {
+        isPedometerAvailable: "checking",
+        pastStepCount: 0,
+        currentStepCount: 0
+    };
+    componentDidMount() {
+        this._subscribe();
+    }
+
+    componentWillUnmount() {
+        this._unsubscribe();
+    }
+
+    _subscribe = () => {
+        this._subscription = Pedometer.watchStepCount(result => {
+            this.setState({
+                currentStepCount: result.steps
+            });
+        });
+
+        Pedometer.isAvailableAsync().then(
+            result => {
+                this.setState({
+                    isPedometerAvailable: String(result)
+                });
+            },
+            error => {
+                this.setState({
+                    isPedometerAvailable: "Could not get isPedometerAvailable: " + error
+                });
+            }
+        );
+
+        const end = new Date();
+        const start = new Date();
+        start.setDate(end.getDate() - 1);
+        Pedometer.getStepCountAsync(start, end).then(
+            result => {
+                this.setState({ pastStepCount: result.steps });
+            },
+            error => {
+                this.setState({
+                    pastStepCount: "Could not get stepCount: " + error
+                });
+            }
+        );
+    };
+
+    _unsubscribe = () => {
+        this._subscription && this._subscription.remove();
+        this._subscription = null;
+    };
+    goBack = () => {
+        this._unsubscribe;
+        this.props.navigation.navigate("home");
+    }
     render() {
-      return (
-        <View>
-            <Text>Step counter</Text>
-        </View>
-      )
-    };
-    
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Steps taken in the last 24 hours: {this.state.pastStepCount}
+                </Text>
+                <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text>
+                <TouchableOpacity
+                    style={{ position: 'absolute', top: 500, left: 100, width: 100, height: 30, backgroundColor: '#ccc' }}
+                    onPress={this.goBack}
+                >
+                    <Text>Go Back</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 15,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+});
